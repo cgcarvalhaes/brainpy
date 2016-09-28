6de650b8444d230911eb996916a87908648545c4
claudio <c.g.carvalhaes@gmail.com>
2016-09-28 08:24:29 -0700

    Fix the url address in setup.py

--- brainpy/setup.py
+++ brainpy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='brainpy',
       version='0.01',
       description='A collection of function to work with EEG data',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/cgcarvalhaes/brainpy.git',
       author='Claudio Carvalhaes',
       author_email='c.g.carvalhaes@gmail.com',
       license='Stanford',
